56710a3c8e202bd53bcede712a1207add65cc309
yahavmor <yahavmor77@gmail.com>
2026-04-05 12:56:03 +0300

    changes after creating a new cluster

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb+srv://yahavmor77_db_user:picoman123@yahavmor.8urmsdo.mongodb.net/BeatifyDB?retryWrites=true&w=majority&appName=yahavMor',
+    dbURL: 'mongodb+srv://yahavmor77_db_user:picoman123@yahavmor.snzbo5w.mongodb.net/BeatifyDB?appName=YahavMor',
     dbName: 'BeatifyDB',
 }
